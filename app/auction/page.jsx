33e643f09fa70564ecb8109959af0ac07d2c48dc
shivadwivedi6891 <122284144+shivadwivedi6891@users.noreply.github.com>
2025-09-02 15:33:41 +0530

    seo check

--- app/auction/page.jsx
+++ app/auction/page.jsx
@@ -2,10 +2,15 @@
 import React, { Suspense } from 'react';
 import AuctionList from './AuctionList';
 
+export const metadata = {
+  title: "Live Auctions | YourSiteName",
+  description: "Browse and bid on live vehicle auctions. Filter by state, brand, and category",
+};
+
+
+
 export default function AuctionPage() {
   return (
-   
-      <AuctionList/>
-
+    <AuctionList/>
   );
 }
